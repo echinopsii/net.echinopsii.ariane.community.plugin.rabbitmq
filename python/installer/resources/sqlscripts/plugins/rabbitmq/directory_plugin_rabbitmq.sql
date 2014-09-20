29173eb139cb591199736ebb58cd584c03853371
Mathilde Ffrench <ffrench.mathilde@gmail.com>
2014-09-20 12:17:01 +0200

    [TAC-142] refactor component to node

--- python/installer/resources/sqlscripts/plugins/rabbitmq/directory_plugin_rabbitmq.sql
+++ python/installer/resources/sqlscripts/plugins/rabbitmq/directory_plugin_rabbitmq.sql
@@ -1,23 +1,40 @@
 --
--- Table structure for table `tibcorvComponent`
+-- Table structure for table `rabbitmqCluster`
 --
 
-CREATE TABLE IF NOT EXISTS `rabbitmqComponent` (
+CREATE TABLE IF NOT EXISTS `rabbitmqCluster` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `description` varchar(255) DEFAULT NULL,
-  `rabbitmqComponentName` varchar(255) DEFAULT NULL,
+  `rabbitmqClusterName` varchar(255) DEFAULT NULL,
+  `version` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_qyf9nbitp86n5gsq3bbqm2fd5` (`rabbitmqClusterName`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+
+--
+-- Table structure for table `rabbitmqNode`
+--
+
+CREATE TABLE IF NOT EXISTS `rabbitmqNode` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `rabbitmqNodeName` varchar(255) DEFAULT NULL,
   `passwd` varchar(255) DEFAULT NULL,
-  `rabbitmqComponentURL` varchar(255) DEFAULT NULL,
+  `rabbitmqNodeURL` varchar(255) DEFAULT NULL,
   `user` varchar(255) DEFAULT NULL,
   `version` int(11) DEFAULT NULL,
+  `cluster_id` bigint(20) DEFAULT NULL,
   `osInstance_id` bigint(20) DEFAULT NULL,
   `supportTeam_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_4j6jq1wnsw9we9r2j6yrcp16y` (`rabbitmqComponentName`,`rabbitmqComponentURL`),
-  UNIQUE KEY `UK_q5xhx6dg8jbhxg7lh8vafsk1w` (`rabbitmqComponentName`),
-  UNIQUE KEY `UK_kpws50q7tb4rn0wv6jickw7c2` (`rabbitmqComponentURL`),
-  KEY `FK_k7tshoh8ml1si2hbef42rgce5` (`osInstance_id`),
-  KEY `FK_q4w1r29c0rmtv7s0pk5brm2wt` (`supportTeam_id`),
-  CONSTRAINT `FK_k7tshoh8ml1si2hbef42rgce5` FOREIGN KEY (`osInstance_id`) REFERENCES `osInstance` (`id`),
-  CONSTRAINT `FK_q4w1r29c0rmtv7s0pk5brm2wt` FOREIGN KEY (`supportTeam_id`) REFERENCES `team` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  UNIQUE KEY `UK_l9f3ywcc5jm65rrip4944t6lr` (`rabbitmqNodeName`,`rabbitmqNodeURL`),
+  UNIQUE KEY `UK_25yajky9j6et1lffh9fehcd2s` (`rabbitmqNodeName`),
+  UNIQUE KEY `UK_lrciprchqj9lv05ettlrf1uq5` (`rabbitmqNodeURL`),
+  KEY `FK_q64u468bp6bcqx0o63jq3jp09` (`cluster_id`),
+  KEY `FK_6lpb2ycv5nunu3d2jf7ibxpwt` (`osInstance_id`),
+  KEY `FK_sn3vreqh5jj6xotsuvgqt2gtv` (`supportTeam_id`),
+  CONSTRAINT `FK_sn3vreqh5jj6xotsuvgqt2gtv` FOREIGN KEY (`supportTeam_id`) REFERENCES `team` (`id`),
+  CONSTRAINT `FK_6lpb2ycv5nunu3d2jf7ibxpwt` FOREIGN KEY (`osInstance_id`) REFERENCES `osInstance` (`id`),
+  CONSTRAINT `FK_q64u468bp6bcqx0o63jq3jp09` FOREIGN KEY (`cluster_id`) REFERENCES `rabbitmqCluster` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
