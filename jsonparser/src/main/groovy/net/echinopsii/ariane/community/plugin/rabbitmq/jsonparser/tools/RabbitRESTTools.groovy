b355be731484e14b483712f25bfc8007d380104c
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2015-04-06 20:19:37 +0200

    [ACPRMQ-7] add standalone parse on tool

--- jsonparser/src/main/groovy/net/echinopsii/ariane/community/plugin/rabbitmq/jsonparser/tools/RabbitRESTTools.groovy
+++ jsonparser/src/main/groovy/net/echinopsii/ariane/community/plugin/rabbitmq/jsonparser/tools/RabbitRESTTools.groovy
@@ -22,6 +22,17 @@ import net.echinopsii.ariane.community.plugin.rabbitmq.jsonparser.serializable.E
 
 class RabbitRESTTools {
 
+    static List<String> getConnectionNames(RabbitNodeToConnect node) {
+        List<String> ret = new ArrayList<String>()
+        def connections_list_req = node.getRestCli().get(path:'/api/connections')
+        if (connections_list_req.status == 200 && connections_list_req.data != null) {
+            connections_list_req.data.each { aconnection ->
+                ret.add((String)aconnection.name)
+            }
+        }
+        return ret;
+    }
+
     static List<String> getConnectionNames(RabbitClusterToConnect cluster) {
         List<String> ret = new ArrayList<String>()
         def connections_list_req = cluster.get('/api/connections')
@@ -33,12 +44,41 @@ class RabbitRESTTools {
         return ret;
     }
 
+    static List<String> getChannelNames(RabbitNodeToConnect node) {
+        List<String> ret = new ArrayList<String>()
+        def channels_list_req = node.getRestCli().get(path:'/api/channels')
+        if (channels_list_req.status == 200 && channels_list_req.data != null) {
+            channels_list_req.data.each { achannel ->
+                ret.add((String) achannel.name);
+            }
+        }
+        return ret;
+    }
+
     static List<String> getChannelNames(RabbitClusterToConnect cluster) {
         List<String> ret = new ArrayList<String>()
         def channels_list_req = cluster.get('/api/channels')
         if (channels_list_req.status == 200 && channels_list_req.data != null) {
             channels_list_req.data.each { achannel ->
                 ret.add((String) achannel.name);
+            }
+        }
+        return ret;
+    }
+
+    static Map<String,List<String>> getExchangeNames(RabbitNodeToConnect node) {
+        Map<String,List<String>> ret = new HashMap<String,List<String>>()
+        def exchanges_list_req = node.getRestCli().get(path:'/api/exchanges')
+        if (exchanges_list_req.status == 200 && exchanges_list_req.data != null) {
+            exchanges_list_req.data.each { anexchange ->
+                if (ret.get(anexchange.vhost)==null)
+                    ret.put((String)anexchange.vhost, new ArrayList<String>())
+            }
+            exchanges_list_req.data.each { anexchange ->
+                if (anexchange.name.equals(""))
+                    ret.get((String)anexchange.vhost).add(ExchangeFromRabbitREST.RABBITMQ_DEFAULT_EXCH_NAME)
+                else
+                    ret.get((String)anexchange.vhost).add((String)anexchange.name)
             }
         }
         return ret;
@@ -62,6 +102,24 @@ class RabbitRESTTools {
         return ret;
     }
 
+    static Map<String, List<String>> getBindingNames(RabbitNodeToConnect node) {
+        Map<String,List<String>> ret = new HashMap<String, List<String>>()
+        def bindings_list_req = node.getRestCli().get(path:'/api/bindings')
+        if (bindings_list_req.status == 200 && bindings_list_req.data != null) {
+            bindings_list_req.data.each { abinding ->
+                if (ret.get(abinding.vhost)==null)
+                    ret.put((String)abinding.vhost, new ArrayList<String>())
+            }
+            bindings_list_req.data.each { abinding ->
+                if (abinding.source.equals(""))
+                    abinding.source = ExchangeFromRabbitREST.RABBITMQ_DEFAULT_EXCH_NAME
+                ret.get((String)abinding.vhost).add((String)abinding.source +
+                        "-[" + (String)abinding.destination_type + "/{" + (String)abinding.routing_key + "," + (String)abinding.properties_key + "}]->"
+                        + (String)abinding.destination)
+            }
+        }
+        return ret;
+    }
 
     static Map<String, List<String>> getBindingNames(RabbitClusterToConnect cluster) {
         Map<String,List<String>> ret = new HashMap<String, List<String>>()
@@ -82,9 +140,39 @@ class RabbitRESTTools {
         return ret;
     }
 
+    static Map<String,List<String>> getQueueNames(RabbitNodeToConnect node) {
+        Map<String,List<String>> ret = new HashMap<String, List<String>>()
+        def queues_list_req = node.getRestCli().get(path:'/api/queues')
+        if (queues_list_req.status == 200 && queues_list_req.data != null) {
+            queues_list_req.data.each { aqueue ->
+                if (ret.get(aqueue.vhost)==null)
+                    ret.put((String)aqueue.vhost, new ArrayList<String>())
+            }
+            queues_list_req.data.each { aqueue ->
+                ret.get((String)aqueue.vhost).add((String)aqueue.name)
+            }
+        }
+        return ret;
+    }
+
     static Map<String,List<String>> getQueueNames(RabbitClusterToConnect cluster) {
         Map<String,List<String>> ret = new HashMap<String, List<String>>()
         def queues_list_req = cluster.get('/api/queues')
+        if (queues_list_req.status == 200 && queues_list_req.data != null) {
+            queues_list_req.data.each { aqueue ->
+                if (ret.get(aqueue.vhost)==null)
+                    ret.put((String)aqueue.vhost, new ArrayList<String>())
+            }
+            queues_list_req.data.each { aqueue ->
+                ret.get((String)aqueue.vhost).add((String)aqueue.name)
+            }
+        }
+        return ret;
+    }
+
+    static List<String> getVhostNames(RabbitNodeToConnect node) {
+        Map<String,List<String>> ret = new HashMap<String, List<String>>()
+        def queues_list_req = node.getRestCli().get(path:'/api/queues')
         if (queues_list_req.status == 200 && queues_list_req.data != null) {
             queues_list_req.data.each { aqueue ->
                 if (ret.get(aqueue.vhost)==null)
